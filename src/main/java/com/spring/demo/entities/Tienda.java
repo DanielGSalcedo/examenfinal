4ce45849303531b650bda53f6a81821ffa8335f3
Daniel Salcedo <johandanielgs@ufps.edu.co>
2025-06-13 10:56:55 -0500

    tienda finish

--- src/main/java/com/spring/demo/entities/Tienda.java
+++ src/main/java/com/spring/demo/entities/Tienda.java
@@ -1,5 +1,23 @@
 package com.spring.demo.entities;
 
-public class Tienda {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tienda {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String direccion;
+    private String uuid;
 }
