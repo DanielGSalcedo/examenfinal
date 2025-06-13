eb929d18eadb7951986770552f560ab6a07d14d0
Daniel Salcedo <johandanielgs@ufps.edu.co>
2025-06-13 10:53:34 -0500

    cliente finish

--- src/main/java/com/spring/demo/entities/Vendedor.java
+++ src/main/java/com/spring/demo/entities/Vendedor.java
@@ -2,29 +2,21 @@ package com.spring.demo.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class cliente {
-
+@AllArgsConstructor
+public class Vendedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String nombre;
-
-	@ManyToOne
-	@JoinColumn(name = "tipo_documento_id")
-	private Integer tipo_documento;
-	
-	
+    private String documento;
+    private String email;
 }
